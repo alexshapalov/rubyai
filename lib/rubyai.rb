62ae692024d432228492c2c94ee9c7a0daba49bf
sanyakosivchuk <kosivcuks@gmail.com>
2024-02-17 23:03:27 +0200

    added faraday gem

--- lib/rubyai.rb
+++ lib/rubyai.rb
@@ -1,12 +1,11 @@
-require 'uri'
-require 'net/http'
+require 'faraday'
 require 'json'
 
 module RubyAI
   class Error < StandardError; end
 
   class Client
-    BASE_URL = URI.parse("https://api.openai.com/v1/chat/completions")
+    BASE_URL = "https://api.openai.com/v1/chat/completions"
 
     def initialize(api_key, messages, temperature: 0.7, model: "gpt-3.5-turbo")
       @api_key = api_key
@@ -18,10 +17,11 @@ module RubyAI
     attr_accessor :api_key, :model, :messages, :temperature
 
     def call
-      response = Net::HTTP.start(BASE_URL.host, BASE_URL.port, use_ssl: true) do |http|
-        request = Net::HTTP::Post.new(BASE_URL.request_uri, header)
-        request.body = body.to_json
-        http.request(request)
+      response = connection.post do |req|
+        req.url BASE_URL
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['Authorization'] = "Bearer #{@api_key}"
+        req.body = body.to_json
       end
 
       JSON.parse(response.body)
@@ -31,17 +31,16 @@ module RubyAI
 
     def body
       {
-        'model': model,
-        'messages': [{"role": "user", "content": messages}],
-        'temperature': temperature
+        'model': @model,
+        'messages': [{"role": "user", "content": @messages}],
+        'temperature': @temperature
       }
     end
 
-    def header
-      {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer #{api_key}"
-      }
+    def connection
+      Faraday.new do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
     end
   end
 end
