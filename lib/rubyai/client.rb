eb5d4f28be5e6dfed68aa3cbd6ee98c617b40ea8
bodyyyya <vbogdanua177@gmail.com>
2024-03-02 15:26:17 +0200

    Extract configuration class

--- lib/rubyai/client.rb
+++ lib/rubyai/client.rb
@@ -1,33 +1,35 @@
-require 'faraday'
-require 'json'
-require_relative 'configuration'
-require_relative 'http'
-
-module RubyAI
-  class Client
-    def initialize(api_key, messages, temperature: 0.7, model: RubyAI::Configuration::DEFAULT_MODEL)
-      @api_key = api_key
-      @messages = messages
-      @temperature = temperature
-      @model = model
-    end
-
-    def call
-      response = connection.post do |req|
-        req.url RubyAI::Configuration::BASE_URL
-        req.headers.merge!(RubyAI::HTTP.build_headers(@api_key))
-        req.body = RubyAI::HTTP.build_body(@messages, @model, @temperature).to_json
-      end
-
-      JSON.parse(response.body)
-    end
-
-    private
-
-    def connection
-      @connection ||= Faraday.new do |faraday|
-         faraday.adapter Faraday.default_adapter
-      end
+require 'faraday' 
+require 'json' 
+require_relative 'configuration' 
+require_relative 'http' 
+ 
+module RubyAI 
+  class Client 
+ 
+    def initialize(client_or_configuration, messages = nil, **options) 
+      if client_or_configuration.is_a?(Configuration) 
+        @configuration = client_or_configuration 
+      else 
+        @configuration = Configuration.new(client_or_configuration, messages, **options) 
+      end 
     end 
-  end
+ 
+    def call 
+      response = connection.post do |req| 
+        req.url RubyAI::Configuration::BASE_URL 
+        req.headers.merge!(RubyAI::HTTP.build_headers(@configuration.api_key)) 
+        req.body = RubyAI::HTTP.build_body(@configuration.messages, @configuration.model, @configuration.temperature).to_json 
+      end 
+ 
+      JSON.parse(response.body) 
+    end 
+ 
+    private 
+ 
+    def connection 
+      @connection ||= Faraday.new do |faraday| 
+         faraday.adapter Faraday.default_adapter 
+      end 
+    end  
+  end 
 end
