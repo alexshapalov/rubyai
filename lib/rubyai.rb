54bf76c597b18a0239a093258726afa17e9c6242
sanyakosivchuk <kosivcuks@gmail.com>
2024-03-03 00:31:20 +0200

    update readme

--- lib/rubyai.rb
+++ lib/rubyai.rb
@@ -4,3 +4,8 @@ require_relative "rubyai/configuration"
 module RubyAI
   class Error < StandardError; end
 end
+
+configuration = RubyAI::Configuration.new("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", "Who is the best chess player in history?")
+ client = RubyAI::Client.new(configuration)
+ result = client.call
+ puts result.dig("choices", 0, "message", "content")
