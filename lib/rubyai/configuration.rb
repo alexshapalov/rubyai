f0dd1a6fb7cd4ca00b817ea8e97f47e9e50fe8fc
sanyakosivchuk <kosivcuks@gmail.com>
2024-10-21 20:04:13 +0300

    Remove outdated gpt models

--- lib/rubyai/configuration.rb
+++ lib/rubyai/configuration.rb
@@ -7,10 +7,8 @@ module RubyAI
 
     MODELS = {
       "gpt-4" => "gpt-4",
-      "gpt-4-0314" => "gpt-4-0314",
       "gpt-4-32k" => "gpt-4-32k",
       "gpt-3.5-turbo" => "gpt-3.5-turbo",
-      "gpt-3.5-turbo-0301" => "gpt-3.5-turbo-0301",
       "text-davinci-003" => "text-davinci-003"
     }
 
